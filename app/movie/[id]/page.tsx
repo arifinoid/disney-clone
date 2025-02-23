a1e44c39f3fa455bc6b7bc5f3421a1d2bee84a46
arifinoid <rohmadarifin007@gmail.com>
2025-02-23 22:00:23 +0700

    feat: adjust detail page of move or tv show

---
+++ app/movie/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { getMovieDetails } from "@/lib/tmdb";
+
+export default async function MovieDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const movie = await getMovieDetails(id);
+
+  return (
+    <div className="p-4 text-white">
+      <h1 className="text-3xl font-bold">{movie.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/w780${movie.backdrop_path}`}
+        className="w-full rounded-lg mt-4"
+      />
+      <p className="mt-4">{movie.overview}</p>
+    </div>
+  );
+}
---
+++ app/tv/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { getTVDetails } from "@/lib/tmdb";
+
+export default async function TVDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const tv = await getTVDetails(id);
+
+  return (
+    <div className="p-4 text-white">
+      <h1 className="text-3xl font-bold">{tv.name}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/w780${tv.backdrop_path}`}
+        className="w-full rounded-lg mt-4"
+      />
+      <p className="mt-4">{tv.overview}</p>
+    </div>
+  );
+}
