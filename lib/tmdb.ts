7f8e66f668071ad75a2b5dde516b6516673e9e52
arifinoid <rohmadarifin007@gmail.com>
2025-02-23 21:59:33 +0700

    feat: use debounce

--- lib/tmdb.ts
+++ lib/tmdb.ts
@@ -1,3 +1,10 @@
+import {
+  MovieResponse,
+  SearchResult,
+  TopRatedMovieResponse,
+  TopRatedTVShowResponse,
+  TVShowResponse,
+} from "@/types/api";
 import axios from "axios";
 
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
@@ -9,11 +16,28 @@ export const tmdb = axios.create({
 });
 
 export const getTopRatedMovies = async () => {
-  const res = await tmdb.get("/movie/top_rated");
+  const res = await tmdb.get<TopRatedMovieResponse>("/movie/top_rated");
   return res.data.results;
 };
 
 export const getTopRatedTVShows = async () => {
-  const res = await tmdb.get("/tv/top_rated");
+  const res = await tmdb.get<TopRatedTVShowResponse>("/tv/top_rated");
   return res.data.results;
+};
+
+export const searchMulti = async (query: string) => {
+  const res = await tmdb.get<SearchResult>("/search/multi", {
+    params: { query },
+  });
+  return res.data.results;
+};
+
+export const getTVDetails = async (id: string) => {
+  const res = await tmdb.get<TVShowResponse>(`/tv/${id}`);
+  return res.data;
+};
+
+export const getMovieDetails = async (id: string) => {
+  const res = await tmdb.get<MovieResponse>(`/movie/${id}`);
+  return res.data;
 };
